a8af4d89f2f60195d0d2c2e877b9a520fabd5217
Will Smith <willsmithte@gmail.com>
2018-01-20 00:23:16 +1100

    register & login buttons working using stateprovider

--- ang.js
+++ ang.js
@@ -1,21 +1,42 @@
-angular.module('app', ['ui.router'])
-    .controller('homeController', function ($scope) {
-        $scope.show = false;
-        $scope.register = function () {
-            if ($scope.show) {
-                $scope.show = false;
-            } else {
-                $scope.show = true;
-            }
-        }
-    }).controller('registerController', function($scope) {
-        
-    }).config(function ($stateProvider) {
-        $stateProvider.state('root', {
-            templateUrl: 'index.html'
-        }).state('root.register', {
-            templateUrl: 'register.html'
-        }).state('root.login', {
-            templateUrl: 'login.html'
-        })
+var app = angular.module('app', ['ui.router']);
+
+app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function ($stateProvider, $locationProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider.state('/home', {
+        url: '/home',
+        templateUrl: 'home.html',
+        controller: 'homeController'
+    }).state('register', {
+        cache: false,
+        url: '/register',
+        templateUrl: 'register.html'
+    }).state('login', {
+        cache: false,
+        url: '/login',
+        templateUrl: 'login.html'
     });
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+
+}]);
+
+app.controller('homeController', function ($scope) {
+    $scope.visible = false;
+    $scope.show = function () {
+        if ($scope.visible) {
+            $scope.visible = false;
+        } else {
+            $scope.visible = true;
+        }
+    }
+
+}).controller('registerController', function ($scope) {
+
+}).controller('loginController', function ($scope) {
+
+});
+
